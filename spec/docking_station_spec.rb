ce8c2892d5cbdf9bbb1297525ed5f9a61dc3eb5d
BartJudge <BartJudge@gmail.com>
2016-09-29 14:32:39 +0100

    doubles and stubs added

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -15,10 +15,11 @@ describe DockingStation do
   end
 
   describe '#release_bike' do
+
     it 'should release a working bike' do
-    bike = Bike.new
-    subject.dock(bike)
-    expect(subject.release_bike).to eq bike
+      bike = double(:bike, broken?: false)
+      subject.dock(bike)
+      expect(subject.release_bike).to be bike
     end
 
     it 'raises an error when there are no bikes available' do
@@ -26,8 +27,8 @@ describe DockingStation do
     end
 
     it 'should not release a broken bike' do
-      bike = Bike.new
-      bike.report_broken
+      bike = double(:bike)
+      allow(bike).to receive(:broken?).and_return(true)
       subject.dock(bike)
       expect{ subject.release_bike }.to raise_error 'Bike is broken'
     end
@@ -35,19 +36,21 @@ describe DockingStation do
   end
 
   describe '#dock' do
+    let(:bike) { Bike.new }
+
     it 'docks something' do
-      bike = Bike.new
       expect(subject.dock(bike)).to eq [bike]
     end
+
     it 'returns docked bikes' do
-      bike = Bike.new
+
       subject.dock(bike)
       # expect(subject.bike).to eq bike
     end
 
     it 'raises and error when full' do
-      subject.capacity.times { subject.dock Bike.new }
-      expect { subject.dock Bike.new }.to raise_error 'Docking Station full'
+      subject.capacity.times { subject.dock double :bike }
+      expect { subject.dock double(:bike) }.to raise_error 'Docking Station full'
     end
 
   end
